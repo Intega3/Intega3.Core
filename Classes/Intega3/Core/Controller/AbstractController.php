7aabb7aa85a9dc02b79da228229767765d304bdb
Hacksch <martintepper@arcor.de>
2015-07-19 19:19:40 +0200

    move layouts, partials, templates and translations to Intega3.Template set dependency to Intega3.Template

--- Classes/Intega3/Core/Controller/AbstractController.php
+++ Classes/Intega3/Core/Controller/AbstractController.php
@@ -8,12 +8,23 @@ namespace Intega3\Core\Controller;
 
 use TYPO3\Flow\Annotations as Flow;
 
+/**
+ * Abstract Controller for the FUGRM.Base package
+ *
+ * @Flow\Scope("singleton")
+ */
 class AbstractController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
     /**
      * @var \TYPO3\Flow\I18n\Locale
      */
     protected $lang;
+
+    /**
+     * @var \TYPO3\Flow\Package\PackageManagerInterface
+     * @Flow\Inject
+     */
+    protected $packageManager;
 
 	/**
      * @var \TYPO3\Flow\Persistence\PersistenceManagerInterface
@@ -34,9 +45,15 @@ class AbstractController extends \TYPO3\Flow\Mvc\Controller\ActionController {
      * @return void
      */
     public function initializeView(\TYPO3\Flow\Mvc\View\ViewInterface $view) {
+        // Language detection
         $detector   = new \TYPO3\Flow\I18n\Detector();
         $this->lang = $detector->detectLocaleFromHttpHeader($_SERVER["HTTP_ACCEPT_LANGUAGE"]);
         $view->assign('langDetector', $this->lang);
+        
+        $packagePath = $this->packageManager->getPackage('Intega3.Template')->getPackagePath();
+        $view->setTemplateRootPaths(array($packagePath . 'Resources/Private/Templates'));
+        $view->setLayoutRootPath($packagePath .'Resources/Private/Layouts');
+        $view->setPartialRootPath($packagePath . 'Resources/Private/Partials/');
     }
 
     /**
